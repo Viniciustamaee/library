90b91de1b68e1ea9ae8695bdf3143463742ad90f
Vinicius Tamae <vinicius.tamae@pedbot.com.br>
2024-02-07 23:51:56 -0300

    começo do beck

---
+++ backEnd/database/db.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql2')
+
+const con = mysql.createConnection({
+    host: 'mysql',
+    user: 'root',
+    password: '1234',
+    database: 'library'
+});
+
+con.connect(function (err) {
+    if (err) {
+        console.error('Error connecting to MySQL database:', err);
+    } else {
+        console.log('Connected to MySQL database');
+    }
+});
+
+module.exports = con
