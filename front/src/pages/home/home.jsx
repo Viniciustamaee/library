0500eeac045d51a3ee778fd33d57b060288cadf0
PEdbot <vinicius.tamae@pedbot.com.br>
2024-03-25 17:03:19 -0300

    mudei o nome

--- front/src/pages/home/home.jsx
+++ front/src/pages/home/home.jsx
@@ -5,7 +5,7 @@ export default function home() {
     return (
         <div className='text-white' id='container'>
             <div id="content">
-                <h1 className='text-4xl font-normal text-white-900 '>Wellcome the Páginas Infinitas</h1>
+                <h1 className='text-4xl font-normal text-white-900 '>Wellcome the Infinity page</h1>
                 <p className='text-4xl font-thin text-white-900 text-center m-5'>A website where you can rent books</p>
                 <p className='text-4xl font-thin text-white-900 text-center'><a href="/about" className='hover:text-violet-600 '>Click here</a> for more information</p>
             </div>
